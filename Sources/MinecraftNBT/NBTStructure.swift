5fc0a23264c4e5eb98e5552d5dd36f825316f107
Ezekiel Elin <ezekielelin@me.com>
2019-09-04 22:08:55 -0400

    Re-added Gzip

--- Sources/MinecraftNBT/NBTStructure.swift
+++ Sources/MinecraftNBT/NBTStructure.swift
@@ -7,14 +7,15 @@
 
 import Foundation
 import DataTools
+import Gzip
 
 public class NBTStructure {
     public let tag: NBTTag
     
     public init(_ url: URL) throws {
-//        let compressedData = try Data(contentsOf: url)
-//        let data = try compressedData.gunzipped()
-        let data = try Data(contentsOf: url)
+        let compressedData = try Data(contentsOf: url)
+        let data = try compressedData.gunzipped()
+//        let data = try Data(contentsOf: url)
         let stream = DataStream(data)
         
         self.tag = try makeTag(from: stream)
--- Sources/MinecraftTools/main.swift
+++ Sources/MinecraftTools/main.swift
@@ -3,6 +3,6 @@ import Foundation
 import DataTools
 import MinecraftNBT
 
-let structure = try NBTStructure(URL(fileURLWithPath: "/Users/ezekielelin/temp_dev/level.dat.decompressed"))
+let structure = try NBTStructure(URL(fileURLWithPath: "/Users/ezekielelin/temp_dev/level.dat"))
 
 structure.tag.display(indented: 0)
